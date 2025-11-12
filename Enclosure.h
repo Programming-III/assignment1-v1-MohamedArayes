7ef2eea22b47af9a3b0d63870abb0f0c5620b1fe
MohamedArayes <mohamedamro2006.school@gmail.com>
2025-11-12 15:15:36 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,17 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
-
 #include "Animal.h"
 
-//define enclosure class here 
-
-
-
-
-
-
-
+class Enclosure {
+private:
+    Animal** animals;
+    int capacity;
+    int currentCount;
+public:
+    Enclosure(int cap);
+    ~Enclosure();
+    void addAnimal(Animal* a);
+    void displayAnimals() const;
+};
 
 #endif
