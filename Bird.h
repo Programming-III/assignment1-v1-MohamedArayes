00e815c5b1b032547aeae8738d54406a12d02d12
MohamedArayes <mohamedamro2006.school@gmail.com>
2025-11-12 15:15:18 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,15 @@
 #ifndef BIRD_H
 #define BIRD_H
-
 #include "Animal.h"
 
-//define class bird here 
-
-
-
-
-
-
+class Bird : public Animal {
+private:
+    float wingSpan;
+public:
+    Bird();
+    Bird(string n, int a, bool hungry, float w);
+    ~Bird();
+    void display() const override;
+};
 
 #endif
