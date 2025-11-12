da125edcb7b15be2f5b05c18cef29353a31821ed
MohamedArayes <mohamedamro2006.school@gmail.com>
2025-11-12 15:16:34 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,15 @@
 #ifndef REPTILE_H
 #define REPTILE_H
-
 #include "Animal.h"
 
-//define reptile class here
-
-
-
-
-
-
-
-
+class Reptile : public Animal {
+private:
+    bool isVenomous;
+public:
+    Reptile();
+    Reptile(string n, int a, bool hungry, bool venom);
+    ~Reptile();
+    void display() const override;
+};
 
 #endif
